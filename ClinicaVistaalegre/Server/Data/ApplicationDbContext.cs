f45c98af6a33dde5fbb9332409c19f97e2a8b433
Javi Álvarez <javieralvarezcu@gmail.com>
2022-04-21 10:12:28 +0200

    Revert "base de datos creada"
    This reverts commit df9aa04bb5e531fabe0b99c5365447a7d4d4fb50.

--- ClinicaVistaalegre/Server/Data/ApplicationDbContext.cs
+++ ClinicaVistaalegre/Server/Data/ApplicationDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using ClinicaVistaalegre.Server.Models;
-using ClinicaVistaalegre.Shared.Models;
 using Duende.IdentityServer.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
@@ -13,45 +12,6 @@ namespace ClinicaVistaalegre.Server.Data
             DbContextOptions options,
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
-
-
         }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Cita>()
-            .HasOne(p => p.Paciente)
-            .WithMany(c => c.Citas)
-            .HasForeignKey(p => p.PacienteId);
-
-            modelBuilder.Entity<Cita>()
-            .HasOne(p => p.Medico)
-            .WithMany(c => c.Citas)
-            .HasForeignKey(p => p.MedicoId);
-
-
-            modelBuilder.Entity<Mensaje>()
-            .HasOne(p => p.Paciente)
-            .WithMany(c => c.Mensajes)
-            .HasForeignKey(p => p.PacienteId);
-
-            modelBuilder.Entity<Mensaje>()
-            .HasOne(p => p.Medico)
-            .WithMany(c => c.Mensajes)
-            .HasForeignKey(p => p.MedicoId);
-
-            modelBuilder.Entity<Mensaje>()
-                .HasKey(c => new {
-                    c.PacienteId,
-                    c.MedicoId
-                });
-        }
-
-        public DbSet<Cita> Citas { get; set; }
-        public DbSet<Mensaje> Mensajes { get; set; }
-        public DbSet<Medico> Medicos { get; set; }
-        public DbSet<Paciente> Pacientes { get; set; }
     }
 }
